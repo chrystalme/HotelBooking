706c55d1bb1803df73828994f5251876367531ff
Afam Ifeanyi(Chrys) M <afam_ifeanyi@live.com>
2022-11-29 16:54:10 +0200

    Add Update and Delete method

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -16,8 +16,30 @@ router.post('/', async (req, res) => {
 });
 
 //Update a hotel same as PUT /api/hotels/:id
+router.put('/:id', async (req, res) => {
+  try {
+    const updatedHotel = await Hotel.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.send(updatedHotel).status(200);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
 // Delete a hotel same as DELETE /api/hotels/:id
+router.delete('/:id', async (req, res) => {
+  try {
+    await Hotel.findByIdAndDelete(req.params.id);
+    res.status(200).json('Hotel has been deleted');
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
 // Get all hotels same as GET /api/hotels == GET ALL
 
