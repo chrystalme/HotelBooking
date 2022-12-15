8a7a412dda9a89d469eaf852180707a02fe60ea8
Afam Ifeanyi(Chrys) M <afam_ifeanyi@live.com>
2022-12-15 13:30:41 +0200

    Add bcrypt and jsonwebtoken for authentication

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import { registerUser } from '../controllers/authController.js';
+import { loginUser, registerUser } from '../controllers/authController.js';
 
 const router = express.Router();
 
-// Create a new   User same as POST /api/register
+// Create a new   User same as POST /api/auth/register
 router.post('/register', registerUser);
+// Create a new   User same as POST /api/auth/login
+router.post('/login', loginUser);
 
 export default router;
 // module.exports = router;
